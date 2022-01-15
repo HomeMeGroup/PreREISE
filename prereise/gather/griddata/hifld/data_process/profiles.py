fa9d06bdcc52725e59e8d3199adb658aec0149c1
Daniel Olsen <daniel@breakthroughenergy.org>
2022-01-14 17:22:45 -0800

    feat: add function to build hydro profiles

--- prereise/gather/griddata/hifld/data_process/profiles.py
+++ prereise/gather/griddata/hifld/data_process/profiles.py
@@ -1,5 +1,8 @@
+import pandas as pd
+
 from prereise.gather.griddata.hifld import const
 from prereise.gather.griddata.hifld.data_access.load import get_eia_form_860
+from prereise.gather.hydrodata.eia.fetch_historical import get_generation
 from prereise.gather.solardata.nsrdb.sam import retrieve_data_individual
 
 
@@ -59,11 +62,46 @@ def build_solar(nrel_email, nrel_api_key, solar_plants, **solar_kwargs):
     return profiles
 
 
-def build_hydro(eia_api_key, hydro_plants, **hydro_kwargs):
+def build_hydro(eia_api_key, hydro_plants, year, **hydro_kwargs):
     """Use plant-level data to build hydro profiles.
 
+    :param str eia_api_key: API key.
+    :param pandas.DataFrame hydro_plants: data frame of hydro generators.
+    :param int/str year: year of data to fetch and build profiles for.
+    :param dict hydro_kwargs: keyword arguments to pass to
+        :func:`from prereise.gather.hydrodata.eia.fetch_historical.get_generation`.
     :return: (*pandas.DataFrame*) -- data frame of normalized power profiles. The index
         is hourly timestamps for the profile year, the columns are plant IDs, the values
         are floats.
     """
-    
+    # Build shapes for each region
+    query_regions = set(hydro_plants["Balancing Authority Code"].unique())
+    query_regions.add("US48")  # total U.S. hydro profile, to be used as a fall-back
+    generation = get_generation(eia_api_key, regions=query_regions, **hydro_kwargs)
+    # Build shape for 'residual' region (lower 48, minus found regions)
+    found_sum = generation.drop("US48", axis=1).sum(axis=1)
+    generation["residual"] = generation["US48"] - found_sum
+    # Normalize regional totals by regional capacities
+    matching_regions = set(query_regions) & set(generation.columns)
+    ba_groups = hydro_plants.groupby("Balancing Authority Code")
+    ba_capacities = ba_groups["Pmax"].sum()
+    residual_capacity = hydro_plants.loc[
+        ~hydro_plants["Balancing Authority Code"].isin(matching_regions), "Pmax"
+    ].sum()
+    normalized_profiles = (
+        generation[matching_regions] / ba_capacities.loc[matching_regions]
+    )
+    normalized_profiles["residual"] = generation["residual"] / residual_capacity
+    # Build the output dataframe, and populate it
+    profiles = pd.DataFrame(
+        index=pd.data_range(start=f"{year}-01-01-00", end=f"{year}-12-31-23", freq="H"),
+        columns=hydro_plants.index,
+    )
+    for region in query_regions:
+        plant_ids = ba_groups.get_group(region).index
+        if region in normalized_profiles:
+            profiles[plant_ids] = normalized_profiles[region]
+        else:
+            profiles[plant_ids] = normalized_profiles["residual"]
+
+    return profiles
